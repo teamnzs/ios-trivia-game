f8102fcc5bf50ecf230508685b02754540844264
wassupnari <wassupnari@gmail.com>
2016-11-19 12:54:40 -0800

    Added number picker

--- ios-trivia-game/ViewControllers/CreateGameViewController.swift
+++ ios-trivia-game/ViewControllers/CreateGameViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class CreateGameViewController: UIViewController {
+class CreateGameViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var numberOfPlayersPicker: UIPickerView!
+    
+    var numOfPlayersPickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupPickerData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +26,26 @@ class CreateGameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setupPickerData() {
+        self.numberOfPlayersPicker.delegate = self
+        self.numberOfPlayersPicker.dataSource = self
+        self.numOfPlayersPickerData = ["1", "2", "3", "4", "5"]
+    }
+    
+    // The number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return numOfPlayersPickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return numOfPlayersPickerData[row]
+    }
 
     /*
     // MARK: - Navigation
