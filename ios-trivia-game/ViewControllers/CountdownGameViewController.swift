05fe0f31ff7229ef0c0f1099a70e7c3e2f002920
wassupnari <wassupnari@gmail.com>
2016-11-27 16:02:43 -0800

    Added countdownTimer

--- ios-trivia-game/ViewControllers/CountdownGameViewController.swift
+++ ios-trivia-game/ViewControllers/CountdownGameViewController.swift
@@ -10,15 +10,40 @@ import UIKit
 
 class CountdownGameViewController: UIViewController {
 
+    @IBOutlet weak var countdownLabel: UILabel!
+    
+    fileprivate var timerCount = 30
+    fileprivate var countdownTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc fileprivate func updateCounter() {
+        timerCount -=  1
+        countdownLabel.text = ""
+        
+        
+        if (timerCount > 0) {
+            countdownLabel.text = "\(timerCount)sec"
+        }
+        else {
+            countdownTimer.invalidate()
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let destinationIdentifier = Constants.MAIN_TAB_VIEW_CONTROLLER
+            
+            let destination = storyboard.instantiateViewController(withIdentifier: destinationIdentifier)
+            self.present(destination, animated: true, completion: nil)
+        }
     }
     
 
