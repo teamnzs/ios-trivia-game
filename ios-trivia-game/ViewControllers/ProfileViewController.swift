8c320acc540a123bbcd1c6b92adf5834f558b159
Savio Tsui <stsui@expedia.com>
2016-11-28 23:10:42 -0800

    circular image for friends list

--- ios-trivia-game/ViewControllers/ProfileViewController.swift
+++ ios-trivia-game/ViewControllers/ProfileViewController.swift
@@ -71,33 +71,3 @@ class ProfileViewController: UIViewController {
     */
 
 }
-
-extension UIImage {
-    
-    func circularImage(size: CGSize?) -> UIImage {
-        let newSize = size ?? self.size
-        
-        let minEdge = min(newSize.height, newSize.width)
-        let size = CGSize(width: minEdge, height: minEdge)
-        
-        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
-        let context = UIGraphicsGetCurrentContext()
-        
-        self.draw(in: CGRect(origin: CGPoint.zero, size: size), blendMode: .copy, alpha: 1.0)
-        
-        context!.setBlendMode(.copy)
-        context!.setFillColor(UIColor.clear.cgColor)
-        
-        let rectPath = UIBezierPath(rect: CGRect(origin: CGPoint.zero, size: size))
-        let circlePath = UIBezierPath(ovalIn: CGRect(origin: CGPoint.zero, size: size))
-        rectPath.append(circlePath)
-        rectPath.usesEvenOddFillRule = true
-        rectPath.fill()
-        
-        let result = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return result!
-    }
-    
-}
