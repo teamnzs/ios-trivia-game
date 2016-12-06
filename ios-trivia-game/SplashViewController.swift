619504263802226e1ad4d8ed62e4dd421117822b
wassupnari <wassupnari@gmail.com>
2016-12-05 23:38:28 -0800

    Added SplashScreen

---
+++ ios-trivia-game/SplashViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SplashViewController.swift
+//  ios-trivia-game
+//
+//  Created by Nari Shin on 12/5/16.
+//  Copyright © 2016 Team NZS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor(hexString: Constants.TRIVIA_NAVY)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
