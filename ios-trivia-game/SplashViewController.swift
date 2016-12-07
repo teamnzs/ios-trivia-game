18ad02e503fafd58288e2053fef23f4ba6560e9f
wassupnari <wassupnari@gmail.com>
2016-12-07 00:06:45 -0800

    Added SplashScreen

--- ios-trivia-game/SplashViewController.swift
+++ ios-trivia-game/SplashViewController.swift
@@ -13,6 +13,21 @@ class SplashViewController: UIViewController {
     
     override func viewDidLoad() {
         view.backgroundColor = UIColor(hexString: Constants.TRIVIA_NAVY)
+        
+        if (User.currentUser != nil) {
+            print("logged in")
+            DispatchQueue.main.async {
+                [unowned self] in
+                self.performSegue(withIdentifier: Constants.SPLASH_TO_MAIN_SEGUE, sender: nil)
+            }
+        } else {
+            print("not logged in")
+            DispatchQueue.main.async {
+                [unowned self] in
+                self.performSegue(withIdentifier: Constants.SPLASH_TO_LOGIN_SEGUE, sender: nil)
+            }
+        }
+        
     }
     
     override func didReceiveMemoryWarning() {
